867a4c424f8a959a15d678f4cc269418867f74b9
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2023-02-28 23:45:45 +0100

    created Sign In page and added logic to sign in to account

---
+++ src/sign-in.logic.php
@@ -0,0 +1,79 @@
+<?php
+
+
+session_start();
+
+if (isset($_POST['email'])) {
+    //! validation success
+    $passedAll = true;
+
+    function validationFail($errorName, $errorTxt)
+    {
+        global $passedAll;
+        $passedAll = false;
+        $_SESSION[$errorName] =  $errorTxt;
+    }
+
+    //! check email
+    $email = $_POST['email'];
+    $emailB = filter_var($email, FILTER_SANITIZE_EMAIL);
+
+    if (!$email) {
+        validationFail("e_email", "Please provide value");
+    }
+
+    if ($email && filter_var($email, FILTER_VALIDATE_EMAIL) == false || $emailB !== $email) {
+        validationFail("e_email", "Check your email value");
+    }
+
+    //! check password
+    $password = $_POST['password'];
+
+    if (!$password) {
+        validationFail("e_password", "Please provide value");
+    }
+
+    if ($password && (strlen($password) < 6 || strlen($password) > 20)) {
+        validationFail("e_password", "Must have from 3 to 20 characters");
+    }
+
+    //! Remember input values
+    $_SESSION['input_email'] = $email;
+    $_SESSION['input_password'] = $password;
+
+    if ($passedAll) {
+
+        require_once "./config/database.php";
+
+        try {
+            //! check email in DB
+            $sql = 'SELECT * FROM users WHERE email=?';
+            $stmt = $pdo->prepare($sql);
+            $stmt->execute([$email]);
+            $result = $stmt->fetchAll();
+
+
+            if (count($result) === 0) {
+                validationFail("e_email", "Invalid credentials");
+                validationFail("e_password", "Invalid credentials");
+            } else {
+                $user = $result[0];
+
+                if (password_verify($password, $user->password)) {
+                    $_SESSION['signInSuccess'] = true;
+                    $_SESSION['user_id'] = $user->id;
+                    $_SESSION['user_name'] = $user->name;
+                    $_SESSION['user_lastName'] = $user->lastName;
+                    $_SESSION['user_email'] = $user->email;
+                    $_SESSION['user_trial'] = $user->trial;
+                    header("Location: home-page.php");
+                } else {
+                    validationFail("e_email", "Invalid credentials");
+                    validationFail("e_password", "Invalid credentials");
+                }
+            }
+        } catch (Exception $e) {
+            echo "<p class='error'>Server error. Please, try again later</p>";
+        }
+    }
+}
---
+++ src/sign-in.php
@@ -0,0 +1,42 @@
+<?php require_once "sign-in.logic.php" ?>
+<?php include "./inc/head.php" ?>
+<?php require_once "utils.php" ?>
+
+
+
+<header>
+    <div class="logo no-select">mytutor</div>
+    <a href='sign-up.php'><span class='sign-in'>sign up</span></a>
+</header>
+
+<main class="sign-in">
+
+    <div class="form-data">
+
+        <form action="sign-in.php" method="POST">
+            <h2>Sign in to your account</h2>
+
+            <div>
+                <input type="email" name="email" placeholder="Email Address" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" autocomplete="off" value="<?php rememberInputValue('input_email') ?>">
+                <?php showInputError("e_email") ?>
+            </div>
+            <div>
+                <input type="password" name="password" placeholder="Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" autocomplete="off" value="<?php rememberInputValue('input_password') ?>">
+                <?php showInputError("e_password") ?>
+
+            </div>
+            <button type="submit">Sign In</button>
+            <p>By clicking the button, you are agreeing to our <a href="#">Terms and Services</a></p>
+        </form>
+    </div>
+
+    <div class="cta">
+        <h1>Learn at your own pace <br>
+            with hands-on creative classes.</h1>
+        <p>With real world projects to create and online classes that fit a busy routine, <strong>MYTUTOR</strong> makes real learning and growth possible.</p>
+        <p>Start, switch or advance your career and degrees from world-class universities and companies.</p>
+    </div>
+</main>
+
+
+<?php include "./inc/footer.php" ?>
