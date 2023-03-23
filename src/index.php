a9debf1631d73a995cdba2e1aef3e14dab37c81b
Double-w-B <75247773+Double-w-B@users.noreply.github.com>
2023-03-23 23:01:03 +0100

    created router

--- src/index.php
+++ src/index.php
@@ -1,46 +1,4 @@
-<?php require_once "sign-up.logic.php" ?>
-<?php require_once "utils.php" ?>
-<?php include "./inc/head.php" ?>
+<?php
 
-<header>
-    <div class="logo no-select">mytutor</div>
-    <a href='sign-in.php'><span class='sign-in'>sign in</span></a>
-</header>
-
-<main class="sign-up">
-
-    <div class="cta">
-        <h1>Learn to code by <br>
-            watching others</h1>
-        <p>See how experienced developers solve problems in real-time. Watching scripted tutorials is great,
-            but understanding how developers think is invaluable.</p>
-    </div>
-
-    <div class="form-data">
-        <div class="form-data-trial">
-            <p>Try it free 7 days <span>then $20/mo. thereafter</span></p>
-        </div>
-        <form action="index.php" method="POST">
-            <div>
-                <input type="text" name="name" placeholder="Name" onfocus="this.placeholder=''" onblur="this.placeholder='Name'" autocomplete="off" value="<?php rememberInputValue('input_name') ?>">
-                <?php showInputError("e_name") ?>
-            </div>
-            <div>
-                <input type="text" name="lastName" placeholder="Last Name" onfocus="this.placeholder=''" onblur="this.placeholder='Last Name'" autocomplete="off" value="<?php rememberInputValue('input_lastName') ?>">
-                <?php showInputError("e_lastName") ?>
-            </div>
-            <div>
-                <input type="email" name="email" placeholder="Email Address" onfocus="this.placeholder=''" onblur="this.placeholder='Email Address'" autocomplete="off" value="<?php rememberInputValue('input_email') ?>">
-                <?php showInputError("e_email") ?>
-            </div>
-            <div>
-                <input type="password" name="password" placeholder="Password" onfocus="this.placeholder=''" onblur="this.placeholder='Password'" autocomplete="off" value="<?php rememberInputValue('input_password') ?>">
-                <?php showInputError("e_password") ?>
-            </div>
-            <button type="submit">Claim your free trial</button>
-            <p>By clicking the button, you are agreeing to our <a href="#">Terms and Services</a></p>
-        </form>
-    </div>
-
-</main>
-<?php include "./inc/footer.php" ?>
+require "utils.php";
+require "router.php";
---
+++ src/router.php
@@ -0,0 +1,26 @@
+<?php
+
+$url = parse_url($_SERVER['REQUEST_URI'])['path'];
+
+$routes = [
+    "/" => "controllers/registration.php",
+    "/login" => "controllers/login.php",
+    "/tutorials" => "controllers/tutorials.php",
+    "/account" => "controllers/account.php",
+    "/my-tutorials" => "controllers/my-tutorials.php",
+    "/subscription" => "controllers/subscription.php",
+    "/account" => "controllers/account.php",
+];
+
+
+function routeToController($url, $routes)
+{
+    if (array_key_exists($url, $routes)) {
+        require $routes[$url];
+    } else {
+        echo "router problem";
+        exit();
+    }
+}
+
+routeToController($url, $routes);
